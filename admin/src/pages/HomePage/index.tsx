637ae48630ed9104493936b36deccb4935312d2c
Luuk <luuk@luuk.computer>
2024-03-04 17:29:12 +0100

    layout

--- admin/src/pages/HomePage/index.tsx
+++ admin/src/pages/HomePage/index.tsx
@@ -12,6 +12,8 @@ import {
   BaseHeaderLayout,
   ContentLayout,
   Button,
+  ActionLayout,
+  Tag,
   JSONInput,
 } from "@strapi/design-system";
 import axios from "../../utils/axios-instance";
@@ -35,34 +37,35 @@ const HomePage = () => {
   return (
     <Box background="neutral100">
       <BaseHeaderLayout title={config?.name} />
-      <ContentLayout>
-        {config?.buttons?.map((button, i) => (
-          <div key={`button-${i}`}>
-            <Button
-              disabled={loading}
-              onClick={() => {
-                setLoading(true);
-                axios
-                  .request({
-                    url: button?.url,
-                    method: button?.method,
-                  })
-                  .then((res) => {
-                    setResponseData(JSON.stringify(res.data, null, 2));
-                    setLoading(false);
-                  })
-                  .catch((res) => {
-                    setResponseData(
-                      "error: " + JSON.stringify(res.data, null, 2)
-                    );
-                    setLoading(false);
-                  });
-              }}
-            >
-              {button?.label}
-            </Button>
-          </div>
+      <ActionLayout
+        startActions={config?.buttons?.map((button, i) => (
+          <Button
+            key={`button-${i}`}
+            disabled={loading}
+            onClick={() => {
+              setLoading(true);
+              axios
+                .request({
+                  url: button?.url,
+                  method: button?.method,
+                })
+                .then((res) => {
+                  setResponseData(JSON.stringify(res.data, null, 2));
+                  setLoading(false);
+                })
+                .catch((res) => {
+                  setResponseData(
+                    "error: " + JSON.stringify(res.data, null, 2)
+                  );
+                  setLoading(false);
+                });
+            }}
+          >
+            {button?.label}
+          </Button>
         ))}
+      />
+      <ContentLayout>
         <Box margin={8}>
           {responseData && <JSONInput value={responseData} minHeight="300px" />}
         </Box>
